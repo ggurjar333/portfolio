e1c44c6baed38cadf3ff37ba15f818036ca1c8c6
Gaurav Gurjar <ggurjar333@gmail.com>
2024-01-24 18:14:36 +0530

    Consolidate netlify attribute in Contact form
    The netlify attribute has been combined with the name attribute in the Contact form for better readability. This change simplifies the syntax without affecting functionality.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -67,8 +67,7 @@ export default function Contact() {
                     </div>
                 </div>
                 <form
-                    netlify
-                    name="contact"
+                    name="contact" netlify
                     onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <input type="hidden" name="form-name" value="contact" />
