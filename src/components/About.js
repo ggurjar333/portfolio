7f77516db0cf8987a79ba5a61d0300b7900a704e
Gaurav Gurjar <ggurjar333@gmail.com>
2024-02-02 09:57:45 +0530

    Update personal bio in About component
    Updated the personal description in About.js component. The changes include a title modification from "Data Consultant" to "Fullstack Consultant" and a slight modification in the description text.

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,10 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="flex-grow md:w-1/2 lg:pr-22 md:pr-16 flex flex-col items-center text-center md:items-start md:text-left mb-16 md:mb-0">
                     <h1 className="title-font text-3xl mb-4 font-medium text-white sm:text-4xl @screen lg {text-5xl}">
-                        Gaurav Gurjar is a Data Consultant. He lives in Dubai.
+                        Gaurav is a Fullstack Consultant based in Dubai.
                     </h1>
                     <p className="mb-8 leading-relaxed ...">
-                        I love to build amazing apps and data pipelines. I aim to reflect my capabilities, experiences, and achievements in the realm of full-stack data engineering.
+                        I build apps and data pipelines. I aim to reflect my capabilities, experiences, and achievements in the realm of full-stack data engineering.
                     </p>
                     <div className="flex justify-center ...">
                         <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-3 focus:outline-none hover:bg-green-600 rounded text-lg ...">
