43c6b04b1292883a568e9b38eade74e17483c93d
Gaurav Gurjar <ggurjar333@gmail.com>
2024-02-25 13:02:52 +0000

    updates about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="flex-grow md:w-1/2 lg:pr-22 md:pr-16 flex flex-col items-center text-center md:items-start md:text-left mb-16 md:mb-0">
                     <h1 className="title-font text-3xl mb-4 font-medium text-white sm:text-4xl @screen lg {text-5xl}">
-                        Gaurav is a Fullstack Consultant based in Dubai.
+                        Gaurav is a Fullstack Data Engineer.
                     </h1>
                     <p className="mb-8 leading-relaxed ...">
                         I build apps and data pipelines. I aim to reflect my capabilities, experiences, and achievements in the realm of full-stack data engineering.
